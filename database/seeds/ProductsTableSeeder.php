8f5e6024df7bfb4520f5891a321c944224c44de9
Bruno Site <contato@brunosite.com>
2017-04-22 20:35:28 -0300

    Products

---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,13 @@
+<?php
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder {
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(){
+
+    }
+}
