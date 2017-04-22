8f9526384db1e827ea7a0d644625fa957be7ca6a
Bruno Site <contato@brunosite.com>
2017-04-22 20:08:29 -0300

    Seeder

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,14 @@
+<?php
+use Delivery\Models\Category;
+use Illuminate\Database\Seeder;
+
+class CategoriesTableSeeder extends Seeder {
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(){
+        factory(Category::class, 10)->create();
+    }
+}
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,14 @@
+<?php
+use Delivery\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder {
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(){
+        factory(User::class, 10)->create();
+    }
+}
