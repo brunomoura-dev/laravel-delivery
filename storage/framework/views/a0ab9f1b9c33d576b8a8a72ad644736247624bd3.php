40e2bb4ae709aa4df39ea4db5fc2cb52b0d452e2
Bruno Moura <contato@brunosite.com>
2017-05-10 02:10:51 -0300

    Implementaçao de pedidos ao painel administrativo parte

--- storage/framework/views/a0ab9f1b9c33d576b8a8a72ad644736247624bd3.php
+++
@@ -1,5 +0,0 @@
-<?php if($errors->any()): ?>
-    <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-        <p class="alert alert-danger"><?php echo e($error); ?></p>
-    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-<?php endif; ?>
